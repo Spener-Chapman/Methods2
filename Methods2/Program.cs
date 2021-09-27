91d2958ccd05b2ff287f7cb323aed0d2a10b9018
Spener-Chapman <spencerchap@hotmail.com>
2021-09-27 13:08:54 -0300

    update

--- Methods2/Program.cs
+++ Methods2/Program.cs
@@ -8,8 +8,37 @@ namespace Methods2
 {
     class Program
     {
+        // declaration
+        static int score;
+        static int health;
+        static void ShowHud()
+        {
+            Console.WriteLine("Health =" + health);
+        }
+
+        static void TakeDamage(int damage)
+        {
+            health = health - damage;
+        }
         static void Main(string[] args)
         {
+            Console.WriteLine("Methods 2:");
+            Console.WriteLine();
+
+            // initialization
+            score = 0;
+            health = 100;
+
+            ShowHud();
+            TakeDamage(10);
+            ShowHud();
+            TakeDamage(35);
+            ShowHud();
+            TakeDamage(50);
+            ShowHud();
+
+
+            Console.ReadKey(true); // wait for exit...
         }
     }
 }
